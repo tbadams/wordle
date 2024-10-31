850862949695ceae0e6b71f0e8b68e275e9a96e9
Trevor Adams <trevora@yelp.com>
2024-10-31 13:51:21 -0700

    formatting helper

--- dictionary.py
+++ dictionary.py
@@ -9,6 +9,10 @@ def present_count(count_dict, filter_fun):
         )
     )
 
+def format_count_item(kv:tuple[str, int]) -> str:
+    return f"{kv[0]}:{kv[1]}"
+
+
 input = open("words.txt", "r")
 words = input.read().splitlines()
 counts = {}
@@ -20,17 +24,15 @@ for word in words:
             blend = f'{last_char}{char}'
             counts[blend] = counts.get(blend, 0) + 1
         last_char = char
-counts_1 = present_count(counts, lambda key: len(key) == 1)
 
+counts_1 = present_count(counts, lambda key: len(key) == 1)
 with open('counts1.txt', 'w') as f:
-    for line in [f"{kv[0]}:{kv[1]}" for kv in counts_1]:
+    for line in [format_count_item(kv) for kv in counts_1]:
         f.write(f"{line}\n")
 
 counts_2 = present_count(counts, lambda key: len(key) == 2)
-
 with open('counts2.txt', 'w') as f:
-    print("2 letter blends, no vowels:")
-    for line in [f"{kv[0]}:{kv[1]}" for kv in counts_2]:
+    for line in [format_count_item(kv) for kv in counts_2]:
         f.write(f"{line}\n")
         if not any(i in vowels for i in line):
             print(line)

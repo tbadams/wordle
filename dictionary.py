db03aae76683e8bbf365b389955c7ab3f19e434b
Trevor Adams <trevora@yelp.com>
2024-10-31 13:38:10 -0700

    helper for data presentation

--- dictionary.py
+++ dictionary.py
@@ -1,5 +1,14 @@
 vowels = ['a', 'e', 'i', 'o', 'u','y']
 
+def present_count(count_dict, filter_fun):
+    return list(
+        sorted(
+            [(key, value) for (key, value) in count_dict.items() if filter_fun(key)],
+            key=lambda x: x[1],
+            reverse = True
+        )
+    )
+
 input = open("words.txt", "r")
 words = input.read().splitlines()
 counts = {}
@@ -11,13 +20,13 @@ for word in words:
             blend = f'{last_char}{char}'
             counts[blend] = counts.get(blend, 0) + 1
         last_char = char
-counts_1 = list(sorted([(key, value) for (key,value) in counts.items() if len(key) == 1], key=lambda x: x[1], reverse = True))
+counts_1 = present_count(counts, lambda key: len(key) == 1)
 
 with open('counts1.txt', 'w') as f:
     for line in [f"{kv[0]}:{kv[1]}" for kv in counts_1]:
         f.write(f"{line}\n")
 
-counts_2 = list(sorted([(key, value) for (key,value) in counts.items() if len(key) == 2], key=lambda x: x[1], reverse = True))
+counts_2 = present_count(counts, lambda key: len(key) == 2)
 
 with open('counts2.txt', 'w') as f:
     print("2 letter blends, no vowels:")
